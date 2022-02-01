348f743a36e36f9aaacaa1f2f0c7eddbefe658d0
davidBorn <david.born25@gmail.com>
2022-01-31 23:22:59 -0700

    adding build folder

---
+++ build/precache-manifest.a2509dc2c510ee842b08cf7ff6400d6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "063133aa2836051d843a",
+    "url": "/myReads/static/css/main.927b252c.chunk.css"
+  },
+  {
+    "revision": "063133aa2836051d843a",
+    "url": "/myReads/static/js/main.9b36eb8a.chunk.js"
+  },
+  {
+    "revision": "7548f9fad876104d4474",
+    "url": "/myReads/static/js/runtime~main.c7443838.js"
+  },
+  {
+    "revision": "629131fb0a4538416041",
+    "url": "/myReads/static/js/2.6eea1899.chunk.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bf6d5a26c2b884626ae0376b98312fb5",
+    "url": "/myReads/index.html"
+  }
+];
